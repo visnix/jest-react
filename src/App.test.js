781377d5768fed8316580a6ff8a9d7e58c5f25ed
visnix <quantumprogr@gmail.com>
2020-01-31 21:17:12 +0800

    feat: use enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('.app-container').length).toBe(1)
+  expect(wrapper.find(".app-container").prop('title')).toBe('hello xy')
 });
